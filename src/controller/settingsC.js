ab0ddf74855528d639d0d8e15e39d5a5cd5f52f3
Minhazur rahman Nishat <cellsweb.com@gmail.com>
2023-03-04 22:44:34 +0600

    Update2

--- src/controller/settingsC.js
+++ src/controller/settingsC.js
@@ -1,22 +1,11 @@
 const Product = require('../model/Product.js');
+const User = require('../model/User.js');
 const Setting = require('./../model/Setting.js')
 const getIndex = async (req, res) => {
     try {
-        const settingsDocument = await Setting.findOne();
-        if (!settingsDocument) {
-            const defaultSettings = {
-                id: 1,
-                ptPrice: 0,
-                pdPrice: 0,
-                rhPrice: 0,
-                usdToAed: 0,
-                gbpToAed: 0,
-            };
-            await Setting.create(defaultSettings);
-        }
-        const settings = await Setting.findOne();
+        const totalUsers = await User.countDocuments();
         const totalDocs = await Product.countDocuments();
-        res.status(200).json({ msg: 'success', settings: settings, totalDocs: totalDocs });
+        res.status(200).json({ msg: 'success', totalUsers: totalUsers, totalDocs: totalDocs });
     } catch (error) {
         res.status(500).json({ err: 'error', });
     }
