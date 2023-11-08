08e05bcaa1ee674ca3389d2cf5cff256b80e6fc8
Minhazur rahman Nishat <cellsweb.com@gmail.com>
2023-11-08 17:35:00 +0600

    08/11/23-4

--- src/controller/v2/user/productC.js
+++ src/controller/v2/user/productC.js
@@ -6,214 +6,106 @@ const Setting = require('./../../../model/Setting.js')
 const fs = require("fs");
 const History = require("../../../model/History")
 
-const getProducts = async (req, res) => {
-    const page = req.query.page || 1;
-    const searchString = req.query.search || '';
-    const type = req.query.type == 'ceramic' ? 'Ceramic' : req.query.type == 'metal' ? 'Metal' : '';
-    const search = searchString.replace(/\s+/g, '')
-    const carBrand = req.query.brand || null;
-    const userId = req.body.requesterId;
-    const liked = req.query.liked || 'false';
-    const match = (liked == 'true') ? ((carBrand) ? {
-        $and: [
-            {
-                $or: [
-                    { name: { $regex: search, $options: "i" } },
-                    { serial: { $regex: search, $options: "i" } },
-                ],
-            },
-            {
-                $or: [
-                    { material: { $regex: type, $options: "i" } },
-                ],
-            }
-        ],
-        // $or: [
-        //     { name: { $regex: search, $options: "i" } },
-        //     { serial: { $regex: search, $options: "i" } },
-        //     { material: { $regex: type, $options: "i" } },
-        // ],
-        deleted: { $ne: true },
-
-        // material: { $regex: type, $options: "i" },
-
-        // material: { $regex: type, $options: "i" },
-        brands: { $in: [mongoose.Types.ObjectId(carBrand)] },
-        likes: { $in: [mongoose.Types.ObjectId(userId)] },
-    } : {
-        likes: { $in: [mongoose.Types.ObjectId(userId)] },
-        // $or: [
-        //     { name: { $regex: search, $options: "i" } },
-        //     { serial: { $regex: search, $options: "i" } }
-        // ],
-        $and: [
-            {
-                $or: [
-                    { name: { $regex: search, $options: "i" } },
-                    { serial: { $regex: search, $options: "i" } },
-                ],
-            },
-            {
-                $or: [
-                    { material: { $regex: type, $options: "i" } },
-                ],
-            }
-        ],
-        deleted: { $ne: true },
-        // material: { $regex: type, $options: "i" },
-    }) : ((carBrand) ? {
-        // $or: [
-        //     { name: { $regex: search, $options: "i" } },
-        //     { serial: { $regex: search, $options: "i" } },
-        // ],
-        $and: [
-            {
-                $or: [
-                    { name: { $regex: search, $options: "i" } },
-                    { serial: { $regex: search, $options: "i" } },
-                ],
-            },
-            {
-                $or: [
-                    { material: { $regex: type, $options: "i" } },
-                ],
-            }
-        ],
-        deleted: { $ne: true },
-        // material: { $regex: type, $options: "i" },
-        brands: { $in: [mongoose.Types.ObjectId(carBrand)] },
-    } : {
-        // $or: [
-        //     { name: { $regex: search, $options: "i" } },
-        //     { serial: { $regex: search, $options: "i" } },
-        // ],
-        $and: [
-            {
-                $or: [
-                    { name: { $regex: search, $options: "i" } },
-                    { serial: { $regex: search, $options: "i" } },
-                ],
-            },
-            {
-                $or: [
-                    { material: { $regex: type, $options: "i" } },
-                ],
-            }
-        ],
-        deleted: { $ne: true },
-        // material: { $regex: type, $options: "i" },
-    })
-    try {
-        const settings = await Setting.findOne()
-        const user = await User.findOne({ _id: req.body.requesterId })
-        const findMyRate = async (currencyCode, base) => {
-            const exchangeRate = await ExchangeRate.findOne({ base: base, }).sort({ createdAt: -1 }).exec();
-            if (exchangeRate && exchangeRate?.rates && exchangeRate?.rates.hasOwnProperty(currencyCode)) {
-                return exchangeRate.rates[currencyCode];
-            } else {
-                return 0;
-            }
-        }
-        const products = await Product.aggregate([
-            {
-                $match: match
-            },
-            {
-                $lookup: {
-                    from: 'brands',
-                    localField: 'brands',
-                    foreignField: '_id',
-                    as: 'brands'
-                }
-            },
-
-            {
-                $sort: { createdAt: -1, _id: -1 }
-            },
-            {
-                $skip: (page - 1) * 50
-            },
-            {
-                $limit: 50
-            }
-
-        ]);
-
-
-        const totalDocs = await Product.countDocuments(match);
-        const pages = Math.ceil(totalDocs / 50)
-        if (products) {
-            res.status(200).send({
-                products: products,
-                lastPage: page * 50 >= totalDocs ? true : false,
-                pages: pages,
-                current: page,
-                settings: {
-                    ptPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.ptPrice : 0,
-                    pdPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.pdPrice : 0,
-                    rhPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.rhPrice : 0,
-                    discount: user?.discount || 0,
-                    gbpToCurrency: await findMyRate(user?.currency || "AED", "GBP"),
-                    usdToCurrency: await findMyRate(user?.currency || "AED", "USD"),
-                    canSeePrice: user ? ['admin', 'editor', 'viewer'].includes(req.body.requesterRole) : false,
-                    currency: user?.currency || "AED",
-                },
-            });
-        } else {
-            res.status(400).send({
-                products: [],
-                lastPage: true,
-                pages: 1,
-                current: 1,
-                settings: {
-                    ptPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.ptPrice : 0,
-                    pdPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.pdPrice : 0,
-                    rhPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.rhPrice : 0,
-                    discount: user?.discount || 0,
-                    gbpToCurrency: await findMyRate(user?.currency || "AED", "GBP"),
-                    usdToCurrency: await findMyRate(user?.currency || "AED", "USD"),
-                    canSeePrice: user ? ['admin', 'editor', 'viewer'].includes(req.body.requesterRole) : false,
-                    currency: user?.currency || "AED",
-                },
-            });
-        }
-    } catch (error) {
-        res.status(500).json({ err: 'error' })
-    }
-}
 // const getProducts = async (req, res) => {
-//     const page = parseInt(req.query.page) || 1;
+//     const page = req.query.page || 1;
 //     const searchString = req.query.search || '';
 //     const type = req.query.type == 'ceramic' ? 'Ceramic' : req.query.type == 'metal' ? 'Metal' : '';
-//     const search = searchString.replace(/\s+/g, '');
+//     const search = searchString.replace(/\s+/g, '')
 //     const carBrand = req.query.brand || null;
 //     const userId = req.body.requesterId;
 //     const liked = req.query.liked || 'false';
+//     const match = (liked == 'true') ? ((carBrand) ? {
+//         $and: [
+//             {
+//                 $or: [
+//                     { name: { $regex: search, $options: "i" } },
+//                     { serial: { $regex: search, $options: "i" } },
+//                 ],
+//             },
+//             {
+//                 $or: [
+//                     { material: { $regex: type, $options: "i" } },
+//                 ],
+//             }
+//         ],
+//         // $or: [
+//         //     { name: { $regex: search, $options: "i" } },
+//         //     { serial: { $regex: search, $options: "i" } },
+//         //     { material: { $regex: type, $options: "i" } },
+//         // ],
+//         deleted: { $ne: true },
 
-//     const match = {
-//         $or: [
-//             { name: { $regex: search, $options: "i" } },
-//             { serial: { $regex: search, $options: "i" } }
+//         // material: { $regex: type, $options: "i" },
+
+//         // material: { $regex: type, $options: "i" },
+//         brands: { $in: [mongoose.Types.ObjectId(carBrand)] },
+//         likes: { $in: [mongoose.Types.ObjectId(userId)] },
+//     } : {
+//         likes: { $in: [mongoose.Types.ObjectId(userId)] },
+//         // $or: [
+//         //     { name: { $regex: search, $options: "i" } },
+//         //     { serial: { $regex: search, $options: "i" } }
+//         // ],
+//         $and: [
+//             {
+//                 $or: [
+//                     { name: { $regex: search, $options: "i" } },
+//                     { serial: { $regex: search, $options: "i" } },
+//                 ],
+//             },
+//             {
+//                 $or: [
+//                     { material: { $regex: type, $options: "i" } },
+//                 ],
+//             }
 //         ],
 //         deleted: { $ne: true },
-//     };
-
-//     if (type) {
-//         match.material = { $regex: type, $options: "i" };
-//     }
-
-//     if (carBrand) {
-//         match.brands = { $in: [mongoose.Types.ObjectId(carBrand)] };
-//     }
-
-//     if (liked === 'true') {
-//         match.likes = { $in: [mongoose.Types.ObjectId(userId)] };
-//     }
-
+//         // material: { $regex: type, $options: "i" },
+//     }) : ((carBrand) ? {
+//         // $or: [
+//         //     { name: { $regex: search, $options: "i" } },
+//         //     { serial: { $regex: search, $options: "i" } },
+//         // ],
+//         $and: [
+//             {
+//                 $or: [
+//                     { name: { $regex: search, $options: "i" } },
+//                     { serial: { $regex: search, $options: "i" } },
+//                 ],
+//             },
+//             {
+//                 $or: [
+//                     { material: { $regex: type, $options: "i" } },
+//                 ],
+//             }
+//         ],
+//         deleted: { $ne: true },
+//         // material: { $regex: type, $options: "i" },
+//         brands: { $in: [mongoose.Types.ObjectId(carBrand)] },
+//     } : {
+//         // $or: [
+//         //     { name: { $regex: search, $options: "i" } },
+//         //     { serial: { $regex: search, $options: "i" } },
+//         // ],
+//         $and: [
+//             {
+//                 $or: [
+//                     { name: { $regex: search, $options: "i" } },
+//                     { serial: { $regex: search, $options: "i" } },
+//                 ],
+//             },
+//             {
+//                 $or: [
+//                     { material: { $regex: type, $options: "i" } },
+//                 ],
+//             }
+//         ],
+//         deleted: { $ne: true },
+//         // material: { $regex: type, $options: "i" },
+//     })
 //     try {
-//         const settings = await Setting.findOne();
-//         const user = await User.findOne({ _id: req.body.requesterId });
-
+//         const settings = await Setting.findOne()
+//         const user = await User.findOne({ _id: req.body.requesterId })
 //         const findMyRate = async (currencyCode, base) => {
 //             const exchangeRate = await ExchangeRate.findOne({ base: base, }).sort({ createdAt: -1 }).exec();
 //             if (exchangeRate && exchangeRate?.rates && exchangeRate?.rates.hasOwnProperty(currencyCode)) {
@@ -234,6 +126,7 @@ const getProducts = async (req, res) => {
 //                     as: 'brands'
 //                 }
 //             },
+
 //             {
 //                 $sort: { createdAt: -1, _id: -1 }
 //             },
@@ -243,31 +136,138 @@ const getProducts = async (req, res) => {
 //             {
 //                 $limit: 50
 //             }
+
 //         ]);
 
-//         const totalDocs = await Product.countDocuments(match);
-//         const pages = Math.ceil(totalDocs / 50);
 
-//         res.status(200).send({
-//             products: products,
-//             lastPage: page * 50 >= totalDocs,
-//             pages: pages,
-//             current: page,
-//             settings: {
-//                 ptPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.ptPrice : 0,
-//                 pdPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.pdPrice : 0,
-//                 rhPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.rhPrice : 0,
-//                 discount: user?.discount || 0,
-//                 gbpToCurrency: await findMyRate(user?.currency || "AED", "GBP"),
-//                 usdToCurrency: await findMyRate(user?.currency || "AED", "USD"),
-//                 canSeePrice: user ? ['admin', 'editor', 'viewer'].includes(req.body.requesterRole) : false,
-//                 currency: user?.currency || "AED",
-//             },
-//         });
+//         const totalDocs = await Product.countDocuments(match);
+//         const pages = Math.ceil(totalDocs / 50)
+//         if (products) {
+//             res.status(200).send({
+//                 products: products,
+//                 lastPage: page * 50 >= totalDocs ? true : false,
+//                 pages: pages,
+//                 current: page,
+//                 settings: {
+//                     ptPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.ptPrice : 0,
+//                     pdPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.pdPrice : 0,
+//                     rhPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.rhPrice : 0,
+//                     discount: user?.discount || 0,
+//                     gbpToCurrency: await findMyRate(user?.currency || "AED", "GBP"),
+//                     usdToCurrency: await findMyRate(user?.currency || "AED", "USD"),
+//                     canSeePrice: user ? ['admin', 'editor', 'viewer'].includes(req.body.requesterRole) : false,
+//                     currency: user?.currency || "AED",
+//                 },
+//             });
+//         } else {
+//             res.status(400).send({
+//                 products: [],
+//                 lastPage: true,
+//                 pages: 1,
+//                 current: 1,
+//                 settings: {
+//                     ptPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.ptPrice : 0,
+//                     pdPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.pdPrice : 0,
+//                     rhPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.rhPrice : 0,
+//                     discount: user?.discount || 0,
+//                     gbpToCurrency: await findMyRate(user?.currency || "AED", "GBP"),
+//                     usdToCurrency: await findMyRate(user?.currency || "AED", "USD"),
+//                     canSeePrice: user ? ['admin', 'editor', 'viewer'].includes(req.body.requesterRole) : false,
+//                     currency: user?.currency || "AED",
+//                 },
+//             });
+//         }
 //     } catch (error) {
-//         res.status(500).json({ err: 'error' });
+//         res.status(500).json({ err: 'error' })
 //     }
-// };
+// }
+const getProducts = async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const searchString = req.query.search || '';
+    const type = req.query.type == 'ceramic' ? 'Ceramic' : req.query.type == 'metal' ? 'Metal' : '';
+    const search = searchString.replace(/\s+/g, '');
+    const carBrand = req.query.brand || null;
+    const userId = req.body.requesterId;
+    const liked = req.query.liked || 'false';
+
+    const match = {
+        $or: [
+            { name: { $regex: search, $options: "i" } },
+            { serial: { $regex: search, $options: "i" } }
+        ],
+        deleted: { $ne: true },
+    };
+
+    if (type !== '') {
+        match.material = { $regex: type, $options: "i" };
+    }
+
+    if (carBrand) {
+        match.brands = { $in: [mongoose.Types.ObjectId(carBrand)] };
+    }
+
+    if (liked === 'true' || liked == true) {
+        match.likes = { $in: [mongoose.Types.ObjectId(userId)] };
+    }
+
+    try {
+        const settings = await Setting.findOne();
+        const user = await User.findOne({ _id: req.body.requesterId });
+
+        const findMyRate = async (currencyCode, base) => {
+            const exchangeRate = await ExchangeRate.findOne({ base: base, }).sort({ createdAt: -1 }).exec();
+            if (exchangeRate && exchangeRate?.rates && exchangeRate?.rates.hasOwnProperty(currencyCode)) {
+                return exchangeRate.rates[currencyCode];
+            } else {
+                return 0;
+            }
+        }
+        const products = await Product.aggregate([
+            {
+                $match: match
+            },
+            {
+                $lookup: {
+                    from: 'brands',
+                    localField: 'brands',
+                    foreignField: '_id',
+                    as: 'brands'
+                }
+            },
+            {
+                $sort: { createdAt: -1, _id: -1 }
+            },
+            {
+                $skip: (page - 1) * 50
+            },
+            {
+                $limit: 50
+            }
+        ]);
+
+        const totalDocs = await Product.countDocuments(match);
+        const pages = Math.ceil(totalDocs / 50);
+
+        res.status(200).send({
+            products: products,
+            lastPage: page * 50 >= totalDocs,
+            pages: pages,
+            current: page,
+            settings: {
+                ptPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.ptPrice : 0,
+                pdPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.pdPrice : 0,
+                rhPrice: (user && ['admin', 'editor', 'viewer'].includes(req.body.requesterRole)) ? settings?.rhPrice : 0,
+                discount: user?.discount || 0,
+                gbpToCurrency: await findMyRate(user?.currency || "AED", "GBP"),
+                usdToCurrency: await findMyRate(user?.currency || "AED", "USD"),
+                canSeePrice: user ? ['admin', 'editor', 'viewer'].includes(req.body.requesterRole) : false,
+                currency: user?.currency || "AED",
+            },
+        });
+    } catch (error) {
+        res.status(500).json({ err: 'error' });
+    }
+};
 
 const likeProduct = async (req, res) => {
     try {
